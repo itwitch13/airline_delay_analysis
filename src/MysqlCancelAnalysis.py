aaa0d1ad4b991625d969a8db5cf2ba9ba807771c
Blania <blaneczka96@gmail.com>
2021-01-24 18:08:58 +0100

    Create statistics for cancelled flights, using mysql and pandas

--- src/MysqlCancelAnalysis.py
+++ src/MysqlCancelAnalysis.py
@@ -7,15 +7,37 @@ from src.airlines_data import airlines
 def get_data(db, query):
     cur = db.cursor()
     cur.execute(query)
-    df = pd.DataFrame(cur.fetchall())
+    df = pd.DataFrame(cur.fetchall()).reset_index().iloc[:, 1:3]
 
     return df
 
+def prepare_data_cancellation(df_city, name):
+    df = pd.DataFrame()
+    name = name.split('_', 1)
+    if 'OFTEN' in name[0]:
+        df = df_city.iloc[:20]
+    elif 'LEAST' in name[0]:
+        df = df_city.iloc[-20:]
+    df.columns = [[name[0], name[0]], [name[1], 'count']]
+    return df
+
 def create_statistics(df_statistics):
-    df_statistics = df_statistics.sort_values('Percentage %')
 
     with pd.ExcelWriter('mysql_statistics/mysql_airline_cancellations_analysis_year_2018.xlsx') as writer:
-        df_statistics.to_excel(writer, sheet_name='Airlines - number of delayed', index=True)
+        df_statistics.to_excel(writer, sheet_name='Airlines - number of cancelled', index=True)
+
+def city_cancellation_statistics(df_origin, df_dest):
+    origin_often_cancelled = prepare_data_cancellation(df_origin, 'OFTEN CANCELLED_ORIGIN')
+    origin_least_cancelled = prepare_data_cancellation(df_origin, 'LEAST CANCELLED_ORIGIN')
+    dest_often_cancelled = prepare_data_cancellation(df_dest, 'OFTEN CANCELLED_DEST')
+    dest_least_cancelled = prepare_data_cancellation(df_dest, 'LEAST CANCELLED_DEST')
+
+    df_statistics = pd.concat([origin_often_cancelled, dest_often_cancelled,
+                    origin_least_cancelled.reset_index().iloc[:, 1:3],
+                    dest_least_cancelled.reset_index().iloc[:, 1:3]],
+                   axis=1)
+
+    return df_statistics
 
 hostname = 'localhost'
 username = 'root'
@@ -26,12 +48,15 @@ db = mysql.connector.connect(host=hostname, user=username, passwd=password, db=d
 
 start_time = time.time()
 # liczby anulowanych lotów wg miast wylotu
-query1 = "SELECT ORIGIN, COUNT(*) as count FROM air18 WHERE CANCELLED >0 GROUP BY ORIGIN ORDER BY count ASC;"
+query1 = "SELECT ORIGIN, COUNT(*) as count FROM air18 WHERE CANCELLED >0 GROUP BY ORIGIN ORDER BY count DESC;"
 # liczby anulowanych lotów wg miast przylotów
-query2 = "SELECT DEST, COUNT(*) as count FROM air18 WHERE CANCELLED >0 GROUP BY DEST ORDER BY count ASC;"
-df_all_flights = get_data(db, query1)
-df_delay_flights = get_data(db, query2)
-db.close()
+query2 = "SELECT DEST, COUNT(*) as count FROM air18 WHERE CANCELLED >0 GROUP BY DEST ORDER BY count DESC;"
+
+origin_cancelled = get_data(db, query1)
+dest_cancelled = get_data(db, query2)
 print("--- {} seconds ---".format(time.time() - start_time))
 
+df_statistics = city_cancellation_statistics(origin_cancelled, dest_cancelled)
+create_statistics(df_statistics)
 
+db.close()
